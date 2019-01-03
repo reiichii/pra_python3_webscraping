a68b3f4dac57e44ac980a24abf5da82871c11129
reiichii <passion.engk@gmail.com>
2019-01-04 01:37:42 +0900

    add work note

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # pra_python3_webscraping
+
+* python3.5
+* requests (2.21.0)
+* Scrapy (1.5.1)
+* beautifulsoup4 (4.7.0)
+
+
+* 成果物: brickset2016.json
+* 設定ファイル: [brickset.py](https://github.com/reiichii/pra_python3_webscraping/lego_scraper/spiders/brickset.py)
+* 実行コマンド
+```
+scrapy crawl brickset -o brickset2016.json
+```
--- lego_scraper/lego_scraper/spiders/brickset.py
+++ lego_scraper/lego_scraper/spiders/brickset.py
@@ -5,13 +5,13 @@ import scrapy
 class BricksetSpider(scrapy.Spider):
     name = 'brickset'
     allowed_domains = ['brickset.com']
-    start_urls = ['https://brickset.com/sets/year-2016']
+    start_urls = ['https://brickset.com/sets/year-2016'] #対処ページのurl
 
     def parse(self, response):
         for brickset in response.css('article.set'):
             meta = brickset.css('div.meta')
             
-            number = meta.css('h1 span::text').re_first(r'(.+): ')
+            number = meta.css('h1 span::text').re_first(r'(.+): ') #re_firstで":"より前を抜き出している
            
             name = brickset.css('div.highslide-caption h1::text').extract_first()
             print(number, name)
@@ -19,8 +19,8 @@ class BricksetSpider(scrapy.Spider):
             yield {
                 'number': number,
                 'name': name,
-            }
+            } #出力するために整形
 
-        next_url = response.css('li.next a::attr(href)').extract_first()
+        next_url = response.css('li.next a::attr(href)').extract_first() #次のページ以降もあれば取得
         if next_url:
             yield scrapy.Request(next_url)
